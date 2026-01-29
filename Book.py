84cb638b6a1e9a32b2c64e923645d762acbbb74f
Aiden Munger <=>
2026-01-29 11:55:21 -0800

    coded all of book and gave an example

--- Book.py
+++ Book.py
@@ -1,3 +1,8 @@
+# Aiden Munger
+# 1/29/2026
+# Book class Demo OOP
+# Library System
+
 class Book:
     def __init__(self, title : str, checked_out : bool = False):
         self.title = title
@@ -5,3 +10,19 @@ class Book:
         
     def check_out(self):
         self.checked_out = True
+    
+    def return_book(self):
+        self.checked_out = False
+
+    def __str__(self):
+        str_rep = f"{self.title}, checked out: {self.checked_out}"
+        
+        return str_rep
+    
+hp1 = Book("Harry Potter and the Sorcerer's Stone")
+print(hp1)
+hp1.check_out()
+print(hp1)
+
+hp1.return_book()
+print(hp1)

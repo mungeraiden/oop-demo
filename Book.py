78a3312fd5ea2244b2fea93f7a28504f73d8e64e
Aiden Munger <=>
2026-02-03 11:48:54 -0800

    Sync

--- Book.py
+++ Book.py
@@ -14,15 +14,34 @@ class Book:
     def return_book(self):
         self.checked_out = False
 
+    def __eq__(self, other: object) -> bool:
+        # We get to define how two Books are compared for equality.
+        return self.title == other.title
+
     def __str__(self):
         str_rep = f"{self.title}, checked out: {self.checked_out}"
         
         return str_rep
     
 hp1 = Book("Harry Potter and the Sorcerer's Stone")
-print(hp1)
-hp1.check_out()
-print(hp1)
+#print(hp1)
+#hp1.check_out()
+#print(hp1)
 
 hp1.return_book()
-print(hp1)
+#print(hp1)
+
+
+book_shelf: list[Book] = [hp1, Book("Harry Potter and the Chamber of Secrets"), Book("Harry Potter and the Prisoner of Azkaban"), Book("Harry Potter and the Goblet of Fire"), Book("Harry Potter and the Order of the Phoenix"), Book("Harry Potter and the Half-Blood Prince"), Book("Harry Potter and the Deathly Hollow")]
+
+for i in range(len(book_shelf)):
+    print(i)
+    book_shelf[i].check_out()
+    print(book_shelf[i])
+    print()
+
+print(hp1 == book_shelf[0])
+print(hp1 is book_shelf[0])
+#hp1_copy = copy.copy(hp1)
+#print(hp1 == hp1_copy)
+#print(hp1 is hp1_copy)
